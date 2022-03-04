283bf9bacbeaff469dc38d17b291a4420aa3b8ae
Gary <58466698+garypach@users.noreply.github.com>
2022-03-03 18:29:11 -0800

    update home page

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MoviesService {
-  private apiUrl='https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2021-11-15&primary_release_date.lte=2021-12-22&api_key=def4f9ac2beb10d1686640969bf52c0b'
+  private apiUrl='https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2021-12-15&primary_release_date.lte=2022-12-22&api_key=def4f9ac2beb10d1686640969bf52c0b'
   public movieUrl=`https://api.themoviedb.org/3/movie/{{slug}}?api_key=def4f9ac2beb10d1686640969bf52c0b&language=en-US`
 
   public get(url: string) {
--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , AfterContentInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { MoviesService } from 'src/app/core/services/movies.service';
 
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,12 +18,71 @@ export class HomeComponent implements OnInit {
     this._movie.getAllMovies().subscribe((movie)=>{
       this.moviesDisplay = movie.results;
       console.log(movie.results)
-    })
 
+    });
+
+    $('.single-item').slick({
+      slidesToShow: 1,
+      dots: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      prevArrow: $('.prev-hero'),
+      nextArrow: $('.next-hero'),    
+      responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+           
+        }
+      }
+    ]
+  })
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,        
+    prevArrow: $('.prev-movie'),
+    nextArrow: $('.next-movie'),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  
   }
 
   clickedMovie(id:number){
     this.router.navigate(['movies',id])
-  }
+  };
+
+
+
+  
 
 }
